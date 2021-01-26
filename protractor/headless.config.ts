0011f0db5ddcabbf357d4bff3f37d35671e3d000
Daniela <dhiguitaa17@gmail.com>
2021-01-26 16:26:01 -0500

    implicitly wait

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,14 +5,19 @@ export const config: Config = {
   framework: "jasmine",
   specs: ["../test/**/*.spec.js"],
   SELENIUM_PROMISE_MANAGER: false,
-  capibilities: {
+  capabilities: {
     browserName: "chrome",
     chromeOptions: {
       args: ["--headless", "--disable-gpu"],
     },
   },
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000,
+  },
   onPrepare: () => {
     reporter();
+    browser.manage().timeouts().implicitlyWait(2000);
     browser.ignoreSynchronization = true;
   },
 };
