23abdb161744cd408de5bab3d81eaab41c100666
Daniela <dhiguitaa17@gmail.com>
2021-01-27 10:22:47 -0500

    split the test in differents describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -11,45 +11,64 @@ import {
   BankPaymentPage,
   OrderSummaryPage,
 } from "../src/page";
-describe("Buy a t-shirt", () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModal: ProductAddedModal = new ProductAddedModal();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStep: SignInStep = new SignInStep();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStep: ShippingStep = new ShippingStep();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
 
-  it("then should be bought a t-shirt", async () => {
+describe("Open the browser", () => {
+  beforeAll(async () => {
     await browser.get("http://automationpractice.com/");
-    await menuContentPage.goToTShirtMenu();
+  });
 
-    //product-list
-    await productListPage.goToItem();
+  describe("I try to buy a t-shirt", () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productListPage: ProductListPage = new ProductListPage();
+    const productAddedModal: ProductAddedModal = new ProductAddedModal();
+    const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+    beforeAll(async () => {
+      await menuContentPage.goToTShirtMenu();
+      //product-list
+      await productListPage.goToItem();
 
-    await browser.sleep(2000);
-    //add to car
-    await productAddedModal.addProduct();
+      await browser.sleep(2000);
+      //add to car
+      await productAddedModal.addProduct();
 
-    //summary
-    await summaryStepPage.nextStep();
-    //sing in
-    await signInStep.completeForm();
-    //address
-    await addressStepPage.selectAddress();
-    //shiping
-    await shippingStep.agreeTerms();
-    await shippingStep.nextStep();
-    //payment step
-    await paymentStepPage.selectPay();
-    //bank
-    await bankPaymentPage.confirmOrder();
+      //summary
+      await summaryStepPage.nextStep();
+    });
 
-    await expect(orderSummaryPage.result()).toBe(
-      "Your order on My Store is complete."
-    );
+    describe("try to ligin", () => {
+      const signInStep: SignInStep = new SignInStep();
+      beforeAll(async () => {
+        //sing in
+        await signInStep.completeForm();
+      });
+
+      describe("select the default address", () => {
+        const addressStepPage: AddressStepPage = new AddressStepPage();
+        const shippingStep: ShippingStep = new ShippingStep();
+        beforeAll(async () => {
+          //address
+          await addressStepPage.selectAddress();
+          //shiping
+          await shippingStep.agreeTerms();
+          await shippingStep.nextStep();
+        });
+
+        describe("payment step", () => {
+          const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+          const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+          const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+          it("then should be bought a t-shirt", async () => {
+            //payment step
+            await paymentStepPage.selectPay();
+            //bank
+            await bankPaymentPage.confirmOrder();
+
+            await expect(orderSummaryPage.result()).toBe(
+              "Your order on My Store is complete."
+            );
+          });
+        });
+      });
+    });
   });
 });
