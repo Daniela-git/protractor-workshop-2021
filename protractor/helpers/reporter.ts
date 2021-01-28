03e5579570a3482f4fcaf93fb1ecc3f57ee2c96a
Daniela <dhiguitaa17@gmail.com>
2021-01-28 10:19:44 -0500

    config jasmine-awesome-report

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,8 +1,20 @@
-import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+import { SpecReporter, StacktraceOption } from "jasmine-spec-reporter";
+// import { AwesomeReport } from "jasmine-awesome-report";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { AwesomeReport } = require("jasmine-awesome-report");
+
 export const reporter = () => {
-  jasmine.getEnv().addReporter(new SpecReporter({
-    spec: {
-      displayStacktrace: StacktraceOption.PRETTY
-    }
-  }));
+  jasmine.getEnv().addReporter(
+    new SpecReporter({
+      spec: {
+        displayStacktrace: StacktraceOption.PRETTY,
+      },
+    })
+  );
+  const config = {
+    fullPath: "awesome-report",
+    fileName: "report",
+    merge: true,
+  };
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
