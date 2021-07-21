c090f7316792f49dc3c0af5a5c472b2b1c2d37ab
Daniela <dhiguitaa17@gmail.com>
2021-07-21 07:23:30 -0500

    improvement in the test

--- test/locators.spec.ts
+++ test/locators.spec.ts
@@ -8,55 +8,34 @@ describe('fill form', () => {
     await browser.get(
       'https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm '
     );
+    await personalInformationPage.fillForm({
+      firstName: 'Alejandro',
+      lastName: 'Perdomo',
+      sex: 'Male',
+      experience: 7,
+      profession: ['Automation Tester'],
+      tools: ['Selenium Webdriver'],
+      continent: 'Africa',
+      commands: [
+        'Browser Commands',
+        'Navigation Commands',
+        'Switch Commands',
+        'Wait Commands',
+        'WebElement Commands',
+      ],
+      file: 'C:\\Users\\daniela.higuitaa\\Desktop\\protractor-workshop-2021\\resources\\git.jpeg',
+    });
   });
-
-  describe('fill form without file', () => {
+  it('then should have a photo', async () => {
+    const files: number = await personalInformationPage.getFiles();
+    expect(files).toBe(1);
+  });
+  describe('fill form with file', () => {
     it('then should be registered', async () => {
-      await personalInformationPage.fillForm({
-        firstName: 'Alejandro',
-        lastName: 'Perdomo',
-        sex: 'Male',
-        experience: 7,
-        profession: ['Automation Tester'],
-        tools: ['Selenium Webdriver'],
-        continent: 'Africa',
-        commands: [
-          'Browser Commands',
-          'Navigation Commands',
-          'Switch Commands',
-          'Wait Commands',
-          'WebElement Commands',
-        ],
-        file: '',
-      });
       await personalInformationPage.submit();
       await expect(personalInformationPage.confirm()).toBe(
         'Selenium - Automation Practice Form'
       );
-    });
-  });
-  describe('fill form with file', () => {
-    it('then should be registered', async () => {
-      await personalInformationPage.fillForm({
-        firstName: 'Alejandro',
-        lastName: 'Perdomo',
-        sex: 'Male',
-        experience: 7,
-        profession: ['Automation Tester'],
-        tools: ['Selenium Webdriver'],
-        continent: 'Africa',
-        commands: [
-          'Browser Commands',
-          'Navigation Commands',
-          'Switch Commands',
-          'Wait Commands',
-          'WebElement Commands',
-        ],
-        file: 'C:\\Users\\daniela.higuitaa\\Desktop\\protractor-workshop-2021\\resources\\git.jpeg',
-      });
-      const files: number = await personalInformationPage.getFiles();
-      await personalInformationPage.submit();
-      expect(files).toBe(1);
     });
   });
 });
