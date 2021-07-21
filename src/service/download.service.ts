0ac0e1a91d222b5ffa692f8fe0013d17e33b262c
Daniela <dhiguitaa17@gmail.com>
2021-07-21 14:30:33 -0500

    created downloadFile service

--- src/service/download.service.ts
+++ src/service/download.service.ts
@@ -1,3 +1,13 @@
-export public async downloadFile(link: string, filename): Promise<void>{
-  
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+export class DownloadService {
+  private temp: string;
+
+  constructor() {
+    this.temp = resolve('temp');
+  }
+
+  public async downloadFile(link: string, filename: string): Promise<void> {
+    writeFileSync(resolve(this.temp, filename), link);
+  }
 }
