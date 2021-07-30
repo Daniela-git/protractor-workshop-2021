ee04fd319103af603bd76cfc5cda1bf2c0c13fc8
Daniela <dhiguitaa17@gmail.com>
2021-07-30 10:38:01 -0500

    some waits

--- src/page/payment-step.page.ts
+++ src/page/payment-step.page.ts
@@ -9,12 +9,8 @@ export class PaymentStepPage {
 
   public async selectPay(): Promise<void> {
     await browser.wait(
-<<<<<<< HEAD
       ExpectedConditions.elementToBeClickable(this.paymentStep),
       5000
-=======
-      ExpectedConditions.elementToBeClickable(this.paymentStep)
->>>>>>> d804a7185b1e4b88853bbad7a41076feabaccaf4
     );
     await this.paymentStep.click();
   }
